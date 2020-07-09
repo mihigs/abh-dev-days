b929289afe4b2b9c1ee4162ecf964687b2253448
BerMu147 <berin.mujcinovic@edu.fit.ba>
2020-07-10 01:38:05 +0200

    Footer,btnSearch,gmaps

---
+++ frontend/app/components/google-map.js
@@ -0,0 +1,15 @@
+// import Component from "@ember/component";
+
+// export default Component.extend({  
+//     container: this.$('.map-canvas')[0],
+//     insertMap: function() {
+//         var options = {
+//             center: new window.google.maps.LatLng(
+//                 this.get('latitude'),
+//                 this.get('longitude')
+//             ),
+//             zoom: 15
+//         };
+//         new window.google.maps.Map(container, options);
+//     }.on('didInsertElement')
+// });
